5131ca6324e079dc7081997923454d2f6494c1e9
ゆうきゃん <ucan.lab.ggl@gmail.com>
2019-09-11 17:13:12 +0900

    feat routes stub

--- src/routes/web.php
+++ src/routes/web.php
@@ -11,6 +11,26 @@
 |
 */
 
+use App\Task;
+use Illuminate\Http\Request;
+
+/**
+ * タスクダッシュボード表示
+ */
 Route::get('/', function () {
-    return view('welcome');
+    //
+});
+
+/**
+ * 新タスク追加
+ */
+Route::post('/task', function (Request $request) {
+    //
+});
+
+/**
+ * タスク削除
+ */
+Route::delete('/task/{task}', function (Task $task) {
+    //
 });
