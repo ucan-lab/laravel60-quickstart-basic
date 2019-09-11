01a90a77008b859d4eb82cc978a67c509f637bae
ゆうきゃん <ucan.lab.ggl@gmail.com>
2019-09-11 17:10:25 +0900

    feat add name to tasks

--- src/database/migrations/2019_09_11_080746_create_tasks_table.php
+++ src/database/migrations/2019_09_11_080746_create_tasks_table.php
@@ -15,6 +15,7 @@ class CreateTasksTable extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('name');
             $table->timestamps();
         });
     }
