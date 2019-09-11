c5d704c5fcb08a47ef1db463081e81dcd870ee14
ゆうきゃん <ucan.lab.ggl@gmail.com>
2019-09-11 17:19:00 +0900

    feat create task

--- src/routes/web.php
+++ src/routes/web.php
@@ -25,7 +25,21 @@ Route::get('/', function () {
  * 新タスク追加
  */
 Route::post('/task', function (Request $request) {
-    //
+    $validator = Validator::make($request->all(), [
+        'name' => 'required|max:255',
+    ]);
+
+    if ($validator->fails()) {
+        return redirect('/')
+            ->withInput()
+            ->withErrors($validator);
+    }
+
+    $task = new Task();
+    $task->name = $request->name;
+    $task->save();
+
+    return redirect('/');
 });
 
 /**
